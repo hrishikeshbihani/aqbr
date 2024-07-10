2f21c386a749e0f162ec1c0cf53c54fbfb897f7d
anubhavkumar-idfy <anubhav.kumar@idfy.com>
2024-07-10 17:11:23 +0530

    Fixes in EVE AQBR (#11)

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from libs.chart_generator import chart_generator, check_chartio_url_response
 from services.openai_request import make_call_to_openai
 from libs.utils import calculate_previous_date_range
 from libs.chart_generator import chart_generator
-
+import datetime
 
 def get_metabase_dashboard_name(product):
   if product == "VS":
@@ -13,10 +13,10 @@ def get_metabase_dashboard_name(product):
     return "1123-eve-aqbr"
 
 
-def get_metabase_dashboard_url(product, current_start_date, current_end_date, ou_id):
+def get_metabase_dashboard_url(product, current_date_range, previous_date_range, ou_id):
   metabase_dashboard_name = get_metabase_dashboard_name(product)
-  previous_start_date, previous_end_date = calculate_previous_date_range(
-      current_start_date.strftime('%Y-%m-%d'), current_end_date.strftime('%Y-%m-%d'))
+  (current_start_date, current_end_date) = current_date_range
+  (previous_start_date, previous_end_date) = previous_date_range
   return "https://metabase.idfy.com/dashboard/{metabase_dashboard_name}?start_date={current_start_date}&end_date={current_end_date}&ouid={ou_id}&timezone=Asia%2FKolkata&previous_start_date={previous_start_date}&previous_end_date={previous_end_date}".format(metabase_dashboard_name=metabase_dashboard_name, current_start_date=current_start_date, current_end_date=current_end_date, ou_id=ou_id, previous_start_date=previous_start_date, previous_end_date=previous_end_date)
 
 
@@ -43,10 +43,16 @@ def render_developer_app():
   product = st.selectbox(
       "Select Product from the list",
       ("VS", "EVE"))
-  current_start_date = st.date_input("Start Date")
-  current_end_date = st.date_input("End Date")
+  current_date_range = st.date_input(
+      'Select current time period',
+      value=(datetime.datetime.now(), datetime.datetime.now())
+  )
+  previous_date_range = st.date_input(
+      'Select previous time period',
+      value=(datetime.datetime.now(), datetime.datetime.now())
+  )
   ou_id = st.text_input("OU ID")
-  if (current_start_date and current_end_date and ou_id):
+  if (current_date_range and previous_date_range and ou_id):
     is_generate = st.button('Let\'s put AI to work !!')
     if (is_generate):
       story_line, graphical_representations = st.tabs(
@@ -54,12 +60,12 @@ def render_developer_app():
 
       with story_line:
         prompt = get_storyline_prompt(product,
-            current_start_date, current_end_date, ou_id)
+                                      current_date_range, previous_date_range, ou_id)
         if len(prompt) > 0:
           openai_output = make_call_to_openai(prompt)
           st.write(openai_output)
           metabase_dashboard_url = get_metabase_dashboard_url(product,
-              current_start_date, current_end_date, ou_id)
+                                                              current_date_range, previous_date_range, ou_id)
           st.link_button("Open Dashboard in Metabase",
                          metabase_dashboard_url)
 
@@ -69,7 +75,7 @@ def render_developer_app():
           (title, card_id) = title_and_card_id
           with st.expander(title):
             jsonl_data = get_jsonl_data_from_card(
-                card_id, current_start_date=current_start_date, current_end_date=current_end_date, ou_id=ou_id)
+                card_id, current_date_range=current_date_range, previous_date_range=previous_date_range, ou_id=ou_id)
             chart_generator_output = chart_generator(jsonl_data, title)
             corrected_url = check_chartio_url_response(chart_generator_output)
             if corrected_url is not None:
