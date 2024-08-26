6fcd009c94b46414fc0fdadf1924a710e7f81bbc
sakshamgupta-idfy <55431425+sakshamgupta-idfy@users.noreply.github.com>
2024-08-26 12:16:03 +0530

    Execute query on metabase button for NLQ (#18)

--- pages/query_genie.py
+++ pages/query_genie.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import time, json
+import base64
+import os
 from services.chat_completion import get_query
 from services.nlq import get_query_nlq, update_user_messages
 
@@ -12,6 +14,22 @@ ou_id = st.text_input("Enter OU ID", value="e7252c77ff4c")
 user_query = st.text_input("Enter Delulu")
 button = st.button("Get solulu")
 
+def add_execute_button(query):
+    payload = {
+        "dataset_query": {
+            "database": os.getenv("METABASE_DATABASE_ID"),
+            "native": { "query": query, },
+            "type": "native"
+        },
+        "display": "table",
+        "parameters": [],
+        "visualization_settings": {}
+    }
+    question_raw = json.dumps(payload).encode("ascii")
+    question = base64.b64encode(question_raw).decode("ascii")
+    base_url = os.getenv("METABASE_BASE_URL")
+    st.link_button("Get Output", f"{base_url}/question?#{question}")
+
 if button:
     query_genie, nlq = st.columns(2)
     with query_genie:
@@ -21,6 +39,7 @@ if button:
         end_time = time.time()
         st.code(query_generated)
         st.info("Time elapsed in seconds {:.2f}s".format(end_time - start_time))
+        add_execute_button(query_generated)
     with nlq:
         start_time = time.time()
         st.subheader("NLQ")
@@ -30,3 +49,4 @@ if button:
         end_time = time.time()
         st.code(generated_query_nlq["query"])
         st.info("Time elapsed in seconds {:.2f}s".format(end_time - start_time))
+        add_execute_button(generated_query_nlq["query"])
