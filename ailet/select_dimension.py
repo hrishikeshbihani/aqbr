2b902a4e822178520a593ed75d1d540d346afe7f
Yogesh Maheshwari <yogesh.maheshwari@idfy.com>
2024-08-27 13:51:09 +0530

    Clickhouse compatible queries

--- ailet/select_dimension.py
+++ ailet/select_dimension.py
@@ -1,10 +1,14 @@
 import xml.etree.ElementTree as ET
-tree = ET.parse('./data/table_metadata.xml')
+tree = ET.parse('./combined_tables.xml')
 root = tree.getroot()
-tables_xml_string = ET.tostring(root, encoding='utf-8', xml_declaration=True).decode('utf-8')
-def select_dimension(user_input,selected_metric,table_schema):
+
+def select_dimension(user_input,selected_metric,selected_table,product_description):
+    selected_table_xml= root.find(f".//table[@name='{selected_table}']")
+    if selected_table_xml is not None:
+    # Convert the table element to a string
+        selected_table_xml = ET.tostring(selected_table_xml, encoding='unicode')
     with open('prompts/select_dimension.txt', 'r') as file:
         system_text = file.read()
-    system_text=system_text.format(table_metadata=tables_xml_string,selected_metric=selected_metric,table_schema=table_schema)
-    user_text = """<question> {user_input} </question> """.format(user_input=user_input,selected_metric=selected_metric,table_schema=table_schema)
-    return system_text, user_text
+    system_text=system_text.format(selected_metric=selected_metric,table_schema=selected_table_xml,product_description=product_description)
+    user_text = """<question> {user_input} </question> """.format(user_input=user_input,selected_metric=selected_metric,table_schema=selected_table_xml)
+    return system_text, user_text ,selected_table_xml
