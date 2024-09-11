c05fa4242bdd1883ecd6281c90772f976ae99b0d
Anubhav Kumar <anubhav.kumar@idfy.com>
2024-09-11 16:56:35 +0530

    Package ID support in VS AQBR

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,26 +21,7 @@ def get_metabase_dashboard_url(product, current_date_range, previous_date_range,
   return "https://metabase.idfy.com/dashboard/{metabase_dashboard_name}?start_date={current_start_date}&end_date={current_end_date}&ouid={ou_id}&timezone=Asia%2FKolkata&previous_start_date={previous_start_date}&previous_end_date={previous_end_date}".format(metabase_dashboard_name=metabase_dashboard_name, current_start_date=current_start_date, current_end_date=current_end_date, ou_id=ou_id, previous_start_date=previous_start_date, previous_end_date=previous_end_date)
 
 
-def render_stable_app():
-  current_start_date = st.date_input("Start Date")
-  current_end_date = st.date_input("End Date")
-  ou_id = st.text_input("OU ID")
-  if (current_start_date and current_end_date and ou_id):
-    is_generate = st.button('Let\'s put AI to work !!')
-    if (is_generate):
-      prompt = get_storyline_prompt(
-          current_start_date, current_end_date, ou_id)
-      if len(prompt) > 0:
-          openai_output = make_call_to_openai(prompt)
-          st.write(openai_output)
-          metabase_dashboard_url = get_metabase_dashboard_url(
-              current_start_date, current_end_date, ou_id)
-          st.link_button("Open Dashboard in Metabase",
-                         metabase_dashboard_url)
-
-
 def render_developer_app():
-  st.text('This is the beta mode and may have some issues. Feel free to test this feature and reach out to developers for any feedback but do not use this app feature for official purposes.')
   product = st.selectbox(
       "Select Product from the list",
       ("VS", "EVE"))
@@ -53,6 +34,9 @@ def render_developer_app():
       value=(datetime.datetime.now(), datetime.datetime.now())
   )
   ou_id = st.text_input("OU ID")
+  package_ids = ""
+  if (product == "VS"):
+    package_ids = st.text_input("Package Ids", "")
   custom_prompt_inputs = []
   with st.expander("Customization Options"):
     custom_prompt_inputs = render_customizable_rules(product)
@@ -64,7 +48,7 @@ def render_developer_app():
 
       with story_line:
         prompt = get_storyline_prompt(product,
-                                      current_date_range, previous_date_range, ou_id, custom_prompt_inputs)
+                                      current_date_range, previous_date_range, ou_id, custom_prompt_inputs, package_ids)
 
         if len(prompt) > 0:
           openai_output = make_call_to_openai(prompt)
@@ -75,18 +59,19 @@ def render_developer_app():
                          metabase_dashboard_url)
 
       with graphical_representations:
-        title_and_card_id_list = get_title_and_card_id(product)
-        for title_and_card_id in title_and_card_id_list:
-          (title, card_id) = title_and_card_id
-          with st.expander(title):
-            jsonl_data = get_jsonl_data_from_card(
-                card_id, current_date_range=current_date_range, previous_date_range=previous_date_range, ou_id=ou_id)
-            chart_generator_output = chart_generator(jsonl_data, title)
-            corrected_url = check_chartio_url_response(chart_generator_output)
-            if corrected_url is not None:
-              st.image(corrected_url)
-            else:
-              st.write("Graph could not generated due to error from source.")
+        pass
+        # title_and_card_id_list = get_title_and_card_id(product)
+        # for title_and_card_id in title_and_card_id_list:
+        #   (title, card_id) = title_and_card_id
+        #   with st.expander(title):
+        #     jsonl_data = get_jsonl_data_from_card(
+        #         card_id, current_date_range=current_date_range, previous_date_range=previous_date_range, ou_id=ou_id)
+        #     chart_generator_output = chart_generator(jsonl_data, title)
+        #     corrected_url = check_chartio_url_response(chart_generator_output)
+        #     if corrected_url is not None:
+        #       st.image(corrected_url)
+        #     else:
+        #       st.write("Graph could not generated due to error from source.")
 
 def main():
   st.title("AQBR Demo")
